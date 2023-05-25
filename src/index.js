cc7dca77e14ec2b370c721228dd0f01d9c91129a
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-05-25 09:32:22 -0600

    lab completed, delete deliverable added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputForm = document.querySelector("form");
+  const todoListAnchor = document.getElementById("list");
+
+  inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const todoItemToAdd = event.target.children[1].value;
+  const containerForItem = document.createElement("li");
+
+  containerForItem.innerText = todoItemToAdd;
+  todoListAnchor.appendChild(containerForItem);
+
+  const itemButton = document.createElement("button");
+
+  itemButton.innerText = "Delete";
+  containerForItem.appendChild(itemButton);
+  itemButton.addEventListener("click", deleteItem);
+  });
+
+  function deleteItem(event) {
+    const itemToDelete = event.target.parentElement;
+    itemToDelete.remove();
+  }
 });
